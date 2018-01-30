c328a20303cb2136a60a06c47e0bd34d562991e6
Kazama7 <giridhar0241@gmail.com>
2018-01-30 08:14:08 +0530

    First commit

---
+++ app/controllers/giridhar_controller.rb
@@ -0,0 +1,7 @@
+class GiridharController < ApplicationController
+
+      def name
+
+      end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'giridhar/name', to: 'giridhar#name'
+
+  root 'giridhar#name'
+end
