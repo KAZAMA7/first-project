21722d3b8ccc6cd780f57ad5823bac9fd95f381a
Kazama7 <giridhar0241@gmail.com>
2018-01-30 17:32:18 +0530

    added show and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+	def index
+    @users = user.all
+    end
+	def show
+		@user = User.find(params[:id])
+	end	
 	def new
 	end
 	def create
-        @user = User.new(params[:user])
-
+        @user = User.new(user_params)
         @user.save
         redirect_to@user
     end
+    private
+       def user_params
+       	   params.require(:user).permit(:First_name, :Last_name, :DOB, :Address, :Phone)
+        end
 end
